5429e7456777900020ce181fba1fea9eb3a0068a
klishinoleg <klishinoleg@gmail.com>
2025-05-03 11:58:08 +0300

    Change kafka workers running and fix bot test awaiting time

--- tests_env/run_tests.bat
+++ tests_env/run_tests.bat
@@ -1,25 +1,22 @@
 @echo off
 setlocal
 
-echo 🚀 Starting required Kafka workers...
+echo Starting required Kafka workers...
 
-set KAFKA_APP=interfaces.event_broker.kafka_chapter:app
-docker compose up -d --no-recreate --scale kafka_worker=5
+docker compose up --scale kafka_worker_chapter=5 -d
+docker compose up --scale kafka_worker_finance=1 -d
 
-set KAFKA_APP=interfaces.event_broker.kafka_finance:app
-docker compose up -d --no-recreate --scale kafka_worker=1
-
-echo ✅ Kafka workers up
+echo Kafka workers up
 
 REM
 timeout /t 5 > nul
 
-echo 🧪 Running tests...
+echo Running tests...
 docker compose up test_runner --build
 
-echo ✅ Tests finished
+echo Tests finished
 
-echo 🧹 Stopping test Kafka workers...
+echo Stopping test Kafka workers...
 docker compose stop kafka_worker
 
 endlocal
